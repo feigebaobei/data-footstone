7e32009d636cb59d422ddec497c44cb101d172b1
feigebaobei <18515195415@163.com>
2020-11-13 12:56:18 +0800

    init

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-import stackTool from './lib/stackTool.js'
+import arrayTool from './lib/arrayTool.js'
+import hashTable from './lib/hashTable.js'
+import linkedListTool from './lib/linkedListTool.js'
+import promiseTool from './lib/promiseTool.js'
+import prototypeExtend from './lib/prototypeExtend.js'
 import queueTool from './lib/queueTool.js'
+import stackTool from './lib/stackTool.js'
 
 export default {
-  stackTool,
-  queueTool
+  arrayTool,
+  hashTable,
+  linkedListTool,
+  promiseTool,
+  prototypeExtend,
+  queueTool,
+  stackTool
 }
