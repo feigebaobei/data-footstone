f7a849e9e2164a438fd2950c31af837a7ac2ab2d
feigebaobei <18515195415@163.com>
2020-11-13 10:31:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,7 @@
+import stackTool from './lib/stackTool.js'
+import queueTool from './lib/queueTool.js'
+
+export default {
+  stackTool,
+  queueTool
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+基本的数据结构。
+
+Stack
+Queue
+LinkedList
+HashTable
+Tree
+Graph
+
